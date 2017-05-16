3a9375a24de2ecdb35d784486428256bb7cc41a1
Ram Iyer <ramakrishnan.rkology@gmail.com>
2017-05-16 08:56:42 +0530

    Cleanup Travis config

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,10 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
-function doCompile {
-  ./script/cibuild
-}
-
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
-    doCompile
+    bundle exec jekyll build
     exit 0
 fi
 
